a1258d7a49f0f4dec6b3736a16118242a9040324
zacanger <zac@zacanger.com>
2019-01-10 22:42:42 -0700

    Add some stuff

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Koa middleware for [lvcnf](https://npmjs.com/package/lvcnf).
 
+WIP, does nothing yet
+
 --------
 
 ## Installation
@@ -10,7 +12,27 @@ Koa middleware for [lvcnf](https://npmjs.com/package/lvcnf).
 
 ## Usage
 
-TODO
+```javascript
+const Koa = require('koa')
+const app = new Koa()
+const conf = require('koa-lvcnf')
+
+// other middlewares
+app.use(conf('/config')) // route prefix
+
+app.listen(8888, () => {
+  console.log('i am an app!')
+})
+```
+
+```
+GET /config => all config values
+GET /config/foo => config for foo
+GET /config/foo.bar.baz => the baz value in { foo: { bar: { baz } } }
+DELETE /config/foo.bar.baz => deletes foo.bar.baz
+POST /config/foo.bar.baz => creates foo.bar.baz
+PATCH /config/foo.bar.baz => updates foo.bar.baz
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -1,9 +1,48 @@
+const conf = require('lvcnf')
+
+const parseKV = (s = '') => s.split('=')
+
 module.exports = (prefix) =>
   async (ctx, next) => {
-    // if ctx.path starts with prefix,
-    // call handler
-    // GET /prefix/foo.bar.baz => get('foo.bar.baz')
-    // DELETE /prefix/foo.bar.baz => del('foo.bar.baz')
-    // put, patch, post
+    const pf = `${prefix}/`.replace(/\/\//, '/')
+    if (ctx.request.path.startsWith(pf)) {
+      const key = ctx.request.path.replace(pf, '')
+      switch (ctx.request.method) {
+        case 'GET':
+          if (key.length) {
+            ctx.body = conf.get(key)
+            return
+          } else {
+            ctx.body = conf.getAll()
+            return
+          }
+        case 'DELETE':
+          // todo: no key handler, no length handler
+          if (pf.length) {
+            conf.delete(pf)
+            ctx.status = 204
+          }
+          ctx.body = null
+          return
+        case 'POST':
+          // todo: no key handler, no length handler
+          if (pf.length) {
+            const kv = parseKV(pv)
+            conf.set(kv[0], kv[1])
+            ctx.status = 201
+            ctx.body = null
+          }
+          return
+        case 'PATCH':
+          // todo: no key handler, no length handler
+          if (pf.length) {
+            const kv = parseKV(pv)
+            conf.set(kv[0], kv[1])
+            ctx.status = 204
+            ctx.body = null
+          }
+          return
+      }
+    }
     return next()
   }
