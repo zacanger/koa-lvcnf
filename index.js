9f28f2553ce61ede70faa753e1d3ea9fc2cc70cd
zacanger <zac@zacanger.com>
2019-01-10 22:16:41 -0700

    add a stub

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = {}
+module.exports = (prefix) =>
+  async (ctx, next) => {
+    // if ctx.path starts with prefix,
+    // call handler
+    // GET /prefix/foo.bar.baz => get('foo.bar.baz')
+    // DELETE /prefix/foo.bar.baz => del('foo.bar.baz')
+    // put, patch, post
+    return next()
+  }
